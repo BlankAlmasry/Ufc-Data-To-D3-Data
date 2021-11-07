964a42a4bf52ff13314a22f4b47c704d897b3730
blankalmasry <blankhussien@gmail.com>
2021-11-07 20:32:21 +0200

    added alot of thigns that some how i forget to commit

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from elote import LambdaArena, GlickoCompetitor, ECFCompetitor, DWZCompetitor, EloCompetitor
 import json
+
+from algorithms.glicko2 import Rating, Glicko2
+
 from matches import compile_matches
-compile_matches(GlickoCompetitor)
+compile_matches(EloCompetitor)
+
