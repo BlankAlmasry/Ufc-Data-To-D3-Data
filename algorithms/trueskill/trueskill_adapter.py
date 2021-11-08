612a2511db1823d0412526ffe1ca70f44c4fcee9
blankalmasry <blankhussien@gmail.com>
2021-11-08 21:22:03 +0200

    fixed passed meethods in trueskilladapter

--- algorithms/trueskill/trueskill_adapter.py
+++ algorithms/trueskill/trueskill_adapter.py
@@ -1,5 +1,5 @@
 from elote.competitors.base import BaseCompetitor
-from trueskill import rate_1vs1, Rating
+from trueskill import rate_1vs1, Rating, quality_1vs1
 
 
 class TrueskillAdapter(BaseCompetitor):
@@ -26,34 +26,10 @@ class TrueskillAdapter(BaseCompetitor):
         competitor._rating = p2
 
     def export_state(self):
-        pass
+        return {
+            "initial_rating": self._rating.mu,
+            "initial_rd": self._rating.sigma,
+        }
 
     def expected_score(self, competitor):
-        pass
-
-# def glicko_2_adapter(fighters, rating, sorted_fights):
-#     players = {}
-#     g = Glicko2()
-#     for match in sorted_fights:
-#         player1, player2, result1, result2, *meta = match
-#         matches.assign_players_to_dict(player1, player2, players, Rating)
-#         p1 = players[player1]
-#         p2 = players[player2]
-#         if result1 == "Win" or result2 == "Lose":
-#             p1, p2 = g.rate_1vs1(p1, p2)
-#             players[player1] = p1
-#             players[player2] = p2
-#         elif result1 == "Lose" or result2 == "Win":
-#             p2, p1 = g.rate_1vs1(p2, p1)
-#             players[player1] = p1
-#             players[player2] = p2
-#         elif result1 == "Draw" or result2 == "Draw":
-#             p1, p2 = g.rate_1vs1(p1, p2, drawn=True)
-#             players[player1] = p1
-#             players[player2] = p2
-#         else:
-#             continue
-#         if player1 in fighters:
-#             matches.write_to_csv(player1, p1.mu, rating)
-#         if player2 in fighters:
-#             matches.write_to_csv(player2, p2.mu, rating)
+        return quality_1vs1(self._rating, competitor._rating)
