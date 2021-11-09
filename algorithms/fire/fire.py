e4b6118800a1e71d443a9965ab18315a1ce8d2f1
blankalmasry <blankhussien@gmail.com>
2021-11-09 18:54:16 +0200

    added fire algorithm class

---
+++ algorithms/fire/fire.py
@@ -0,0 +1,33 @@
+from elote.competitors.base import BaseCompetitor
+
+
+class Fire(BaseCompetitor):
+    def __init__(self, name):
+        self.fire_count = 0
+
+    @property
+    def rating(self):
+        return self.fire_count
+
+    def expected_score(self, competitor):
+        pass
+
+    def beat(self, competitor):
+        self.fire_count += competitor.fire_count
+        competitor.fire_count = 0
+
+    def lost_to(self, competitor):
+        competitor.fire_count += self.fire_count
+        self.fire_count = 0
+
+    def tied(self, competitor):
+        total = self.fire_count - competitor.fire_count
+        if total > 0:
+            self.fire_count -= total/2
+            competitor.fire_count += total/2
+        else:
+            self.fire_count += total/2
+            competitor.fire_count -= total/2
+
+    def export_state(self):
+        pass
